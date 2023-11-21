20dd10543d881f6496d9b44c57aa1f4440574ed6
David Tang <687449+iamdtang@users.noreply.github.com>
2023-11-20 22:07:02 -0800

    Layouts

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,12 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>DVDs</title>
-</head>
-<body>
+@extends("main-layout")
+
+@section("title", "Home")
+
+@section("main")
   <table>
     <thead>
       <tr>
@@ -21,4 +17,4 @@
       @endforeach
     </tbody>
   </table>
-</body>
+@endsection
